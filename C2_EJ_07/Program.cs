e2a7679d6ebfc6e72c3553a73b8208d6eb49d660
Rubirpg <rubi.rpg@gmail.com>
2022-07-27 12:48:45 +0200

    Agregar archivos de proyecto.

---
+++ C2_EJ_07/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C2_Ej_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Me dices tu nombre: ");
+            string nombre = Console.ReadLine(); //leemos el nombre y lo guardamos en la variable nombre
+            Console.WriteLine("Me dices tu ciudad: ");
+            string ciudad = Console.ReadLine(); //leemos la ciudad y lo guardamos en la variable ciudad
+
+            //mostramos por pantalla 
+            Console.WriteLine();
+            Console.WriteLine("Hola {0} bienvenido a {1}.", nombre, ciudad);
+        }
+    }
+}
